cc63c71e71ea3aaf935ace8b59c90ef4322c25a1
Yan Merkushin <merkushinyan@gmail.com>
2019-09-02 17:12:14 +0300

    add new exports

--- projects/ng-gx-split-text/src/public-api.ts
+++ projects/ng-gx-split-text/src/public-api.ts
@@ -3,4 +3,7 @@
  */
 
 export * from './lib/components/ng-gx-split-text/ng-gx-split-text.component';
+export * from './lib/directives/ng-gx-split-text.directive';
+export * from './lib/models/options';
+export * from './lib/models/default-options';
 export * from './lib/ng-gx-split-text.module';
