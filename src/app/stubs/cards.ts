8910a32fcaf69681feecb346b2b0f905e44dd266
Yan Merkushin <merkushinyan@gmail.com>
2019-09-06 16:13:26 +0300

    fix template

--- src/app/stubs/cards.ts
+++ src/app/stubs/cards.ts
@@ -15,8 +15,4 @@ export const cards = [
     name: 'Line-Words',
     text: 'JavaScript often abbreviated as JS, is a high-level, interpreted scripting language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions. Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it, and major web browsers have a dedicated JavaScript engine to execute it.',
   },
-  {
-    name: 'Split-nodes',
-    text: 'JavaScript often abbreviated as JS,<br> is a <span class="color-text">high-level</span>, interpreted scripting language that conforms to the <b>ECMAScript <span style="text-decoration: underline;">specification.</span></b> <br> JavaScript has curly-bracket syntax, dynamic typing,<br> prototype-based object-orientation, and first-class functions. Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it, and major web browsers have a dedicated JavaScript engine to execute it.',
-  },
 ];
